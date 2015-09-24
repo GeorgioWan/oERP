4fd101982003a2361d67a42c3cf14a19541ff848
GeorgioWan <georgio.wan@icatchinc.com>
2015-09-24 14:27:32 +0800

    [ UPDATE ]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :phone, :sex, :password, :password_confirmation, :current_password) }
   end
