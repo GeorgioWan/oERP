88edee276dcc0b4f5b54001e1a2002884dc6205f
GeorgioWan <georgio.wan@icatchinc.com>
2015-10-21 15:45:54 +0800

    [UPDATE]  - add oERP.gif  - rewrite part of application.html.erb with helper

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,4 @@
 class MainController < ApplicationController
-  before_action :authenticate_user!
 
   def home
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def signed_slidebar
+    if user_signed_in?
+      content_tag :div, (render 'shared/slidebar'), class: 'col-sm-2 col-md-2', id: 'oERP-slidebar'
+    end
+  end
+
+  def signed_id
+    return 'oERP-contain' if user_signed_in?
+  end
+
+  def signed_yieldclass
+    value = user_signed_in? ? 'col-sm-7' : (
+      (["/home","/","/about"].include? request.path) ? 'col-sm-6 col-sm-offset-3' : 'col-sm-3 col-md-2 col-sm-offset-5')
+
+    return value
+  end
 end
